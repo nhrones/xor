afe1e96c19c9ccffff9f2954229b908b6e6671c3
NDH <ndhrones@gmail.com>
2025-02-16 09:03:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,16 +8,16 @@ const encrypted = encryptText(rawText, cryptoKey)
 
 log(`"${rawText}" XOR-encrypted = "${encrypted}"`)
 
-// create a Base64-encoded ASCII string from a binary string
+// use (btoa) to create a Base64-encoded ASCII string from a binary value
 const base64 = btoa(encrypted)
-log(`XOR encrypted to base64 = "${base64}", this is best for internet transport`)
+log(`XOR-encrypted to base64 = "${base64}", this is best for internet transport`)
 
 const decrypted = encryptText(encrypted, cryptoKey)
 
-// decode a string of data which has been encoded using Base64 encoding
-log(`from base64 back to XOR = ${atob(base64)}`)
+// use (atob) to decode a string which has been encoded using Base64 encoding
+log(`From base64 back to XOR-encrypted = ${atob(base64)}`)
 
-log(`"XOR ${encrypted}" decrypted = "${decrypted}"
+log(`XOR-encrypted "${encrypted}" to XOR-decrypted = "${decrypted}"
    `)
 
 log(`     Raw    -->        XOR       -->   base64      -->       XOR        -->     raw`)
@@ -29,5 +29,7 @@ log(`
    Given the same key, it will simply reverse the value returned by the original encryption!
    `)
 
-log(`We use Base64 for data transmission.    
-Base64 allows binary data to be transmitted over protocols that only support text, like HTTP and SMTP. `)
+log(`Why Base64    
+We use Base64 strings for data transmission.    
+Base64 allows binary data to be transmitted over protocols that only support text, like HTTP and SMTP. 
+Also, Base64 transmits less bytes than raw binary encoded text.`)
